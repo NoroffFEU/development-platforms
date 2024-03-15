f5ad2981f16b2527a0e83fb434bc44b834f392a5
Jan Henning <triton539@outlook.com>
2024-03-15 10:11:45 +0100

    Add missing directories from sidebar

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,10 +8,10 @@ export default defineConfig({
       title: "Noroff Case Studies",
       description: "Case studies made by Noroff students",
       social: {
-        github: "https://github.com/NoroffFEU/development-platforms/",
+        github: "https://github.com/NoroffFEU/development-platforms/"
       },
       editLink: {
-        baseUrl: "https://github.com/NoroffFEU/development-platforms/edit/main/",
+        baseUrl: "https://github.com/NoroffFEU/development-platforms/edit/main/"
       },
       lastUpdated: true,
       pagination: false,
@@ -20,8 +20,9 @@ export default defineConfig({
         { label: "Akamai", collapsed: true, autogenerate: { directory: "akamai" } },
         { label: "Amazon", collapsed: true, autogenerate: { directory: "amazon" } },
         { label: "BaaS", collapsed: true, autogenerate: { directory: "baas" } },
-        { label: "CMS", collapsed: true, autogenerate: { directory: "cms" } },
         { label: "CDN", collapsed: true, autogenerate: { directory: "cdn" } },
+        { label: "CMS", collapsed: true, autogenerate: { directory: "cms" } },
+        { label: "CodePen", collapsed: true, autogenerate: { directory: "codepen" } },
         { label: "Databases", collapsed: true, autogenerate: { directory: "databases" } },
         { label: "Development Tools", collapsed: true, autogenerate: { directory: "devtools" } },
         { label: "Frameworks & Other tools", collapsed: true, autogenerate: { directory: "frameworks" } },
@@ -32,18 +33,19 @@ export default defineConfig({
         {
           label: "Monitoring & Observability",
           collapsed: true,
-          autogenerate: { directory: "monitoring-observability" },
+          autogenerate: { directory: "monitoring-observability" }
         },
         { label: "OpenAI", collapsed: true, autogenerate: { directory: "openai" } },
-        { label: "TanStack", collapsed: true, autogenerate: { directory: "tanstack" } },
+        { label: "Reverse Proxy", collapsed: true, autogenerate: { directory: "reverse-proxy" } },
         { label: "ServiceNow", collapsed: true, autogenerate: { directory: "servicenow" } },
         { label: "Shopify", collapsed: true, autogenerate: { directory: "shopify" } },
+        { label: "TanStack", collapsed: true, autogenerate: { directory: "tanstack" } },
         { label: "UX Monitoring", collapsed: true, autogenerate: { directory: "ux-monitoring" } },
-        { label: "Web Development & Hosting", collapsed: true, autogenerate: { directory: "webdev-hosting" } },
-      ],
-    }),
+        { label: "Web Development & Hosting", collapsed: true, autogenerate: { directory: "webdev-hosting" } }
+      ]
+    })
   ],
 
   // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
-  image: { service: { entrypoint: "astro/assets/services/sharp" } },
+  image: { service: { entrypoint: "astro/assets/services/sharp" } }
 })
