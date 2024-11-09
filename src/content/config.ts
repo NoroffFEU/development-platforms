c2d5b5d6eb66bc9ae0b6b1b27f0ccfe135dc9e69
Tugba23 <filizakca4@gmail.com>
2024-11-09 21:00:48 +0100

    test

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,14 @@
-import { defineCollection, z } from "astro:content"
-import { docsSchema } from "@astrojs/starlight/schema"
+import { defineCollection, z } from "astro:content";
 
 export const collections = {
   docs: defineCollection({
     type: 'content',
-    schema: docsSchema({
-      extend: z.object(
-        {
-          author: z.string().optional()
-        }
-      )
-    })
-  })
-}
+    schema: z.object({
+      title: z.string(), // Title alanı zorunlu
+      date: z.string().optional(), // Tarih alanı isteğe bağlı
+      author: z.string().optional(), // Author isteğe bağlı
+      keywords: z.array(z.string()).optional(), // Keywords isteğe bağlı
+      tags: z.array(z.string()).optional(), // Tags isteğe bağlı
+    }),
+  }),
+};
