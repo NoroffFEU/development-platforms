f92bb8284751865d7460bc37516dfe64c904ef6b
Tugba23 <filizakca4@gmail.com>
2024-11-09 19:56:37 +0100

    changed content of config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,15 @@
-import { defineCollection, z } from "astro:content"
-import { docsSchema } from "@astrojs/starlight/schema"
+import { defineCollection, z } from "astro:content";
+import { docsSchema } from "@astrojs/starlight/schema";
 
 export const collections = {
   docs: defineCollection({
     type: 'content',
     schema: docsSchema({
-      extend: z.object(
-        {
-          author: z.string().optional()
-        }
-      )
+      extend: z.object({
+        title: z.string(),               // Title alanını zorunlu yapıyoruz
+        date: z.string().datetime(),     // Date alanını zorunlu yapıyoruz
+        author: z.string().optional()    // Author alanını isteğe bağlı bırakıyoruz
+      })
     })
   })
-}
+};
